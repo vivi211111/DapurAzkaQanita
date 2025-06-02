6200a305063dc8bd202e8d92385a079e0e5ef268
Evania Loveola <evanialove15@gmail.com>
2025-06-03 02:30:14 +0700

    eva

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { Menu, X, ShoppingCart } from "lucide-react"
 import { useCart } from "../context/CartContext"
