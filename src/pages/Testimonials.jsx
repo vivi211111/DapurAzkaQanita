e3be5e63a1602911088422bff4afe76654198b95
Evania Loveola <evanialove15@gmail.com>
2025-06-08 12:38:21 +0700

    testi

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -825,7 +825,7 @@ const Testimonials = () => {
             </div>
             <div className="flex justify-center mt-10">
               <span
-                className="block md:hidden  text-batik-gold hover:text-batik-orange font-semibold text-xs cursor-pointer underline underline-offset-4 transition-colors duration-200 flex items-center gap-1"
+                className="block md:hidden  text-batik-gold hover:text-batik-orange font-semibold text-xs cursor-pointer underline underline-offset-4 transition-colors duration-200 items-center gap-1"
                 onClick={() => (window.location.href = "/galeri")}
                 role="button"
                 tabIndex={0}
@@ -838,7 +838,7 @@ const Testimonials = () => {
                 <span className="text-xs font-bold">&gt;</span>
               </span>
               <span
-                className="hidden md:block text-batik-gold hover:text-batik-orange font-semibold text-base cursor-pointer underline underline-offset-4 transition-colors duration-200 flex items-center gap-1"
+                className="hidden md:block text-batik-gold hover:text-batik-orange font-semibold text-base cursor-pointer underline underline-offset-4 transition-colors duration-200 items-center gap-1"
                 onClick={() => (window.location.href = "/galeri")}
                 role="button"
                 tabIndex={0}
